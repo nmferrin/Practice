ae32989e200eedbad2346a8f6f2c509189594af2
nmferrin <nmferrin@gmail.com>
2023-05-18 17:42:29 -0500

    objects enhancements

---
+++ objectsEnhancements.js
@@ -0,0 +1,32 @@
+function createInstructor(firstName, lastName){
+    return {
+        firstName,
+        lastName
+    }
+}
+
+let favoriteNumber = 42;
+let instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+}
+
+
+let teacher = {
+    firstName: "Colt",
+    sayHi(){
+        return "Hi";
+    },
+    sayBye(){
+        return this.firstName + " says bye!";
+    }
+}
+
+function createAnimal(species, verb, noise){
+    return {
+        species,
+        [verb](){
+            return noise;
+        }
+    }
+}
