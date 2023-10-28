6f100a1a42510ff304db881198997fc9bedd680b
Nathanael Ferrin <122507319+nmferrin@users.noreply.github.com>
2023-10-28 15:53:51 -0500

    Add files via upload
    Python Data Structures Exercise

---
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -0,0 +1,21 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    to_swap = to_swap.lowercase()
+    output = ""
+
+    for letter in phrase:
+        if letter.lowercase == to_swap:
+            letter = letter.swapcase
+        output += letter
+    return output
