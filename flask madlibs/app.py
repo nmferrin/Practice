e11bf8a2bf95fa9f19ee9ba2effa23c3c9291650
Nathanael Ferrin <122507319+nmferrin@users.noreply.github.com>
2023-10-31 17:45:53 -0500

    Add files via upload
    madlibs app with flask

---
+++ flask madlibs/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+# from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+# debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def ask_questions():
+    # generate form to request words
+    
+    prompts = story.prompts
+
+    return render_template("prompts.html", prompts=prompts)
+
+@app.route("/story")
+def show_story():
+
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
